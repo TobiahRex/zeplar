2678945852a55962f44337c123139e6544f1411d
Tobiah Rex <tobiah.rex@gmail.com>
2025-12-28 08:25:30 -0800

    Test: verify pre-commit hook auto-fixes and formats code

---
+++ src/test-precommit-hook.ts
@@ -0,0 +1,7 @@
+// Test file for pre-commit hook verification
+// Linting error fixed - unused variable removed
+
+export function testFunction() {
+  console.log("Testing pre-commit hook");
+  return true;
+}
