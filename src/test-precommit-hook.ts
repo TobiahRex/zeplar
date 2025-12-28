dfb27dddc983738ac9e680d5358348eaed1b3a95
Tobiah Rex <tobiah.rex@gmail.com>
2025-12-28 08:25:46 -0800

    Test: remove pre-commit hook verification test file

--- src/test-precommit-hook.ts
+++
@@ -1,7 +0,0 @@
-// Test file for pre-commit hook verification
-// Linting error fixed - unused variable removed
-
-export function testFunction() {
-  console.log("Testing pre-commit hook");
-  return true;
-}
